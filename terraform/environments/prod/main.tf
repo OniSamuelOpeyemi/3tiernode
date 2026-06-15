0c1c7a59959b80a4497ad51c486d8e1d1490cd70
Oni-Opeyemi-Samuel <oniesammy@gmail.com>
2026-06-16 00:24:22 +0100

    Modify Dockerfile

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -75,7 +75,7 @@ module "ecs" {
   db_host            = module.database.cluster_endpoint
   db_name            = var.db_name
   db_secret_arn      = module.database.db_secret_arn
-  aws_region        = var.aws_region
+  aws_region         = var.aws_region
   web_cpu            = var.web_cpu
   web_memory         = var.web_memory
   api_cpu            = var.api_cpu
