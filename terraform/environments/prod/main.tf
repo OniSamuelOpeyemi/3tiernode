db17bcd7325f3758f94d3162615087725e9cc655
Oni-Opeyemi-Samuel <oniesammy@gmail.com>
2026-06-18 10:12:35 +0100

    Change healthcheck from curl to wget in web and api dockerfile

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "3tiernode-emmaus-backend"  # Replace with your aws_s3_bucket
-    key            = "prod/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
+    bucket  = "3tiernode-emmaus-backend" # Replace with your aws_s3_bucket
+    key     = "prod/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
     # use_lockfile   = true  # Replace with your DynamoDB table
   }
 }
